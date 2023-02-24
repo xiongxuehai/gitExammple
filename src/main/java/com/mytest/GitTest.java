2b7ca7dd381cbd842b1dcb2b4d3f8e124c8dad6e
xiongxuehai <126219270+xiongxuehai@users.noreply.github.com>
2023-02-24 23:42:18 +0800

    pull test
    pull

--- src/main/java/com/mytest/GitTest.java
+++ src/main/java/com/mytest/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("master conflict modify");
         System.out.println("hot fix conflict commit");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }

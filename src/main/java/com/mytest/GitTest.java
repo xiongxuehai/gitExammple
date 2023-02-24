f4efad8ab29642b8bcda1b3ff025cb4491d72c5d
xxh <1418026308@qq.com>
2023-02-24 19:16:58 +0800

    first commit

---
+++ src/main/java/com/mytest/GitTest.java
@@ -0,0 +1,12 @@
+package com.mytest;
+
+/**
+ * Hello world!
+ *
+ */
+public class GitTest {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello git!" );
+    }
+}
